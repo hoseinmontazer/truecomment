f183d3a27c74b0e9c45938ea1d16323e4bf44ee9
hosein montazer <hosein.montazer.montazer@gmail.com>
2021-04-01 16:20:12 +0430

    fix some bug and add contact html

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM python:3
 workdir /app
-#ADD trueComment.py 
-#ADD templates/ 
-#ADD static/ 
 COPY app .
+RUN apt-get update
+RUN apt-get install ffmpeg libsm6 libxext6  -y
 RUN pip3 install requests
 RUN pip3 install mysql.connector
 RUN pip3 install pycurl
@@ -11,4 +10,9 @@ RUN pip3 install uuid
 RUN pip3 install asyncio
 RUN pip3 install tornado
 RUN pip3 install git+https://github.com/abenassi/Google-Search-API
+RUN pip3 install pillow
+RUN pip3 install claptcha
+RUN pip3 install captcha
+RUN pip3 install opencv-python
+RUN pip3 install matplotlib
 CMD [ "python3", "-u","./trueComment.py" ]
--- app/trueComment.py
+++ app/trueComment.py
@@ -7,11 +7,28 @@ from bs4 import BeautifulSoup
 import re
 from googleapi import google
 import mysql.connector
-
+import sys
+from PIL import Image, ImageDraw, ImageFont
+import random
+import string
+from PIL import Image
+from claptcha import Claptcha
+from captcha.image import ImageCaptcha
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import time
+import glob
 
 
 define("port", default=8887, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
+
+url=""
+urlName=""
 
 
 class db:
@@ -29,15 +46,40 @@ class db:
 
 class BaseHandler(tornado.web.RequestHandler):
     def get(self):
-        return  self.get_argument("url")
+        global url
+        url= self.get_argument("url")
+        return  url
+
+class captchagenerator(BaseHandler):
+    def gen_cap():
+        n = random.randint(10000,99999)
+        image = ImageCaptcha()
+
+        data = image.generate(str(n))
+        image.write(str(n), './static/captcha/'+str(n)+'.png')
+        #print(n)
+        return n
 
 class searchUrl(BaseHandler):
+    def get(self):
+        n = captchagenerator.gen_cap()
+        print("captcah is;",n)
+        captcha = glob.glob('82822.png')
     def DigikalaUrl(self,url):
-        #url = str(self.get_argument("url"))
-        #ur = url
+        
+        truecomment=[]
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
         name = soup.find(class_="c-product__title")
+        #n = captchagenerator.gen_cap()
+        #print("captcah is;",n)
+        #captcha = Image.open('./static/captcha/'+str(n)+'.png')
+        #captcha = cv2.imread('82822.png')
+        #with open('./static/captcha/'+str(n)+'.png','wb', errors='ignore') as f:
+        #    captcha = f.read()
+        #print(type(captcha))
+        captcha = glob.glob('82822.png')
+        #print("captcha2 is:",captcha)
         if name:
             image = str(soup.find(class_="c-gallery__img"))
             img = re.search("https://(.*?)/?.jpg",image)
@@ -46,17 +88,22 @@ class searchUrl(BaseHandler):
             for string in prud.strings:
                 tit.append(string)
             price = soup.find(class_="c-product__seller-price-real")
-
-            comment ="hi this is fake comment"
+            sql = ("SELECT `id` FROM `url` WHERE `urlName` = '%s' " %(name.text.strip()))
+            selectId = searchUrl.DigikalaSelect(self,sql)
+            for x in selectId :
+                    Id = x[0]
+            sql1 = ("SELECT `comment` FROM `comment` WHERE `id` = '%s'  " %(Id))
+            SqlComment = searchUrl.DigikalaSelect(self,sql1)
+            for c in SqlComment:
+                truecomment.append(str(c[0]))
             i = {"name": name.text.strip(), "image":img.group(),
-                "price": price.text.strip(), "tit":tit,"hi":comment}
+                "price": price.text.strip(), "tit":tit,"hi":truecomment,"captcha":captcha}
+            print(i)
             return i
 
     # select to database
     def DigikalaSelect(self,sql):
         #url = str(self.get_argument("url"))
-        #sql = "SELECT `url`, `id` FROM `url` WHERE `url` = %s "
-        print("select:", sql)
         test = db.connectdb()
         mycursor = test.cursor()
         mycursor.execute(sql)
@@ -80,98 +127,120 @@ class searchUrl(BaseHandler):
     def DigikalaUpdate(self,sql,url,url_id):
         #url = str(self.get_argument("url"))
         ur = url
-        print(url_id)
         test = db.connectdb()
         mycursor = test.cursor()
         mycursor.execute(sql,(url ,url_id, ))
         #myresult = mycursor.fetchall()
         test.commit()
         return 
-                
+
 class InsertComment(BaseHandler):
+
     def get(self):
-        #print(url)
-        #i = searchUrl.DigikalaUrl(self)
-        self.render("comment.html", message= i)
-        pass 
+        comment=""
+        email=""
+        self.render("comment.html",error='', message= i)
+
     def post(self):
-        #print(url)
+        global Id
+        comment = self.get_argument("commentbox")
+        email=  self.get_argument("email")
+        forbiden_words=["سکس","زمین","خدا",]     
         i = searchUrl.DigikalaUrl(self,url)
-        comment = self.get_argument("commentbox")       
-        print(comment)
-        urlName= i["name"]
-        print(urlName)
-        sql = "SELECT `id` FROM `url` WHERE `urlName` = %s "
-        selectId = searchUrl.DigikalaSelect(self,sql,urlName)
-        for x in selectId :
-           Id = x[0]
-           print (Id)
-        sql = "INSERT INTO `comment` (`comment`, `id`) VALUES ( %s, %s )"
-        test = db.connectdb()
-        mycursor = test.cursor()
-        val = (comment,Id)
-        mycursor.execute(sql, val)
-        test.commit()
-        i = searchUrl.DigikalaUrl(self,url)
-        self.render("comment.html", message= i)
-    
+        if  not comment:
+            print ("comment is:",comment)
+            self.render("comment.html",error="لطفانظر خود را وارد کنید" , message=i)
+        elif not email:
+            self.render("comment.html",error="لطفاایمیل خود را وارد کنید" , message=i)
+        else:    
+            for f in forbiden_words:
+                if f in comment:
+                    self.render("comment.html",error="شما از کلمه '%s' نمیتوانید در نظرات استغاده کنید" %(f) , message=i)
+                    break
+                else:
+
+                    sql = ("SELECT `id` FROM `url` WHERE `urlName` = '%s' " %(urlName))
+                    selectId = searchUrl.DigikalaSelect(self,sql)
+                    for x in selectId :
+                        Id = x[0]
+                        print (Id)
+                    sql = "INSERT INTO `comment` (`comment`, `id`, `email`) VALUES ( %s, %s, %s )"
+                    test = db.connectdb()
+                    mycursor = test.cursor()
+                    val = (comment,Id,email)
+                    mycursor.execute(sql, val)
+                    test.commit()
+                    i = searchUrl.DigikalaUrl(self,url)
+                    #self.redirect("comment",message=i)
+                    self.redirect("/")
+
+                    return
+class Contact(BaseHandler):
+   
+    def get(self):
+        self.render("contact.html",error='')
+
+    def post (self):
+        self.render("contact.html")
+
 
 class MainHandler(BaseHandler):
+   
     def get(self):
+        captcha = glob.glob('82822.png')
         self.render("url.html",error='')
-    def post(self):
-        global url
-        url = str(self.get_argument("url"))
-        print(url)
-        # update find  url 
-        if "https://www.digikala.com/" in url:
-            i = searchUrl.DigikalaUrl(self, url)
-            urlName = i ["name"]
-            if urlName:
-                sql = ("SELECT `url`, `id` ,`urlName` FROM `url` WHERE `urlName` = '%s' " %(urlName))
-                myresult = searchUrl.DigikalaSelect(self , sql )
-                print (myresult)
-                if  not myresult:
-                        print("i am here")
-                        searchUrl.DigikalaInsert(self)
-                        i = searchUrl.DigikalaUrl(self,url)
-                        self.render("comment.html", message=i)
-                        print("i am inser name")
-                elif  myresult:
-                    print("i am here!!!!")     
-                    for row in myresult:
-                        if row[2] == urlName:
-                            print("printi : ",i)
-                            self.render("comment.html", title="My title", message=i)
-                            url_id = row[1]
-                            sql = "UPDATE `url` SET `url` = %s  WHERE `url`.`id` = %s ;"
-                            searchUrl.DigikalaUpdate(self, sql , url , url_id)
-                            print(" i am update")
-                    return
-        
-        #find  fast search modual
-        
-        else:
-            print ("hii search")
-            sealink=[]
-            seadec=[]
-            search_results = google.search(url, 3)
-            for result in search_results:
-                    #print(result.link)
-                sealink.append(str(result.link))
-                seadec.append(str(result.description))
 
-            sea = {"link":sealink , "dec":seadec}
-            self.render("search.html", message=(sea))
-        #else:
+    def post (self):
+            global url
+            global urlName
+            url = str(self.get_argument("url"))
+            # update find  url 
+            if "https://www.digikala.com/" in url:
+                i = searchUrl.DigikalaUrl(self, url)
+                urlName = i ["name"]
+                if urlName:
+                    sql = ("SELECT `url`, `id` ,`urlName` FROM `url` WHERE `urlName` = '%s' " %(urlName))
+                    myresult = searchUrl.DigikalaSelect(self , sql )
+                    if  not myresult:
+                            print("i am here")
+                            searchUrl.DigikalaInsert(self)
+                            i = searchUrl.DigikalaUrl(self,url)
+                            self.render("comment.html", message=i)
+                            print("i am insert name")
+                    elif  myresult:
+                        print("i am here!!!!")     
+                        for row in myresult:
+                            if row[2] == urlName:
+                                self.render("comment.html", error='', message=i)
+                                url_id = row[1]
+                                sql = "UPDATE `url` SET `url` = %s  WHERE `url`.`id` = %s ;"
+                                searchUrl.DigikalaUpdate(self, sql , url , url_id)
+                                print(" i am update")
 
-#self.render("url.html", error="please enter anything!")
+                return 
 
+            
+            #find  fast search modual
+            else:
+                print ("hii search")
+                sealink=[]
+                seadec=[]
+                search_results = google.search(url, 3)
+                for result in search_results:
+                        #print(result.link)
+                    sealink.append(str(result.link))
+                    seadec.append(str(result.description))
+                print(sealink)
+                sea = {"link":sealink , "dec":seadec}
+                self.render("search.html", message=(sea))
+           
+            
 def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/comment", InsertComment),
+    (r"/contact",Contact),
     ], cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
